4c420c3fe0faffa89745dff75b72cc4f7da735e5
Alesandro Gonzalez <nelsonalesandrog@gmail.com>
2021-04-18 21:55:55 -0600

    [FIX] Puerto expuesto

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 'use strict';
 
 const express = require('express');
-const PORT = 8080;
+const PORT = 8081;
 const HOST = '0.0.0.0';
 const app = express();
 
 app.get('/', (req, res) => {
-  console.log("[GET] Peticion Recibida sin errores")
-  res.send('Taller Kubernetes URACCAN!');
+  res.send('Running on GKE!');
 });
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
